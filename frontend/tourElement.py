bd3378c4207318921f20aa72327b96203edf03b9
Emilia1603 <62947303+Emilia1603@users.noreply.github.com>
2022-08-15 14:43:00 +0200

    little updates: widget order, gpx calculations

--- frontend/tourElement.py
+++ frontend/tourElement.py
@@ -1,4 +1,5 @@
 import datetime
+from re import L
 import uuid
 import streamlit as st
 import sys
@@ -36,7 +37,7 @@ class TourWidget:
                       "climbs": ""}
 
     # build tour widget
-    def tourWidget(self, tour):
+    def tourWidget(self, tour:dict, typ:str):
 
         # starttime of the tour
         date = datetime.datetime.fromtimestamp(tour['date'])
@@ -47,29 +48,33 @@ class TourWidget:
         # widget
         with st.expander(title, expanded = False):
             
-            # Header 
+            # Organisation Attributes 
+            self.addCaption("Organisation")
             headCols = st.columns(3)
-            headCols[0].metric(label="Starttime", value=str(date))
-            headCols[1].metric(label="Startplace", value=tour["startplace"]) 
-            headCols[2].metric(label="Guide", value=tour["owner"]) 
+
+            headCols[0].write("Starttime")
+            headCols[1].write("Startplace")
+            headCols[2].write("Guide")
+
+            headCols[0].write(str(date))
+            headCols[1].write(tour["startplace"])
+            headCols[2].write(tour["owner"])
+
             st.write("---")
 
-            # Second
+            # Participants
             self.addCaption(f"Participants ({len(tour['participants'])})")
             cols2_1 = st.columns(3)
             for i, p in enumerate(tour["participants"]):
                 j  = helpMod(i, 3)
                 #cols2_1[j].write(p)
-                cols2_1[j].button(label=p, key=f"{p}_{tour['unique']}", disabled=True)
+                cols2_1[j].button(label=p, key=f"{p}_{tour['unique']}_{typ}", disabled=True)
 
             # add participate option
-            self.participate(tour)
+            self.participate(tour, typ)
 
             # Sum overall
-
             st.write("---")
-
-            # Third
             self.addCaption("The hard facts")
             cols3 = st.columns(3)
             for i, at in enumerate(self.units):
@@ -77,7 +82,7 @@ class TourWidget:
                 cols3[j].metric(label=at, value= f"{tour[at]} {self.units[at]}")
             st.write("---")
 
-            # Last: GPX Data
+            #  GPX Data
             self.addCaption("GPX")
             gpxCols = st.columns([1,4,1])
             with gpxCols[1]:
@@ -85,15 +90,25 @@ class TourWidget:
 
                 self.gpx.showGpx(gpxData)
 
-            # The 
+            # Editing
+            if st.session_state["login"]["credents"]["user"] == tour["owner"]:
+                st.write("---")
+                self.addCaption("Editing")
+                cols4 = st.columns(3)
+                cols4[-1].button(label="Delete this tour", on_click=self.deleteCallback, args=(tour, ), key=f"delete_{tour['unique']}_{typ}")
 
     # build widget for new tour creation
     def createWidget(self):
         with st.container():
-            tourAttr = {}
+            # initials
+            tourAttr = {}  # collect tour attributes
+            tempGpx = {}  # collect gpx calculated attributes
 
             # Title
             tourAttr["title"] = st.text_input("Title")
+
+            # default title if no title inserted
+            tourAttr["title"] = "no title" if len(tourAttr["title"]) == 0 else tourAttr["title"]
 
             # Header
             headCols = st.columns(3)
@@ -103,16 +118,27 @@ class TourWidget:
 
             tourAttr["startplace"] = headCols[2].text_input(label="start place")
 
-            # Second 
-            cols2 = st.columns(3)
-            for i, at in enumerate(sorted(self.attributes)):
-                j = helpMod(i, 3)
-                tourAttr[at] = cols2[j].text_input(label=at)
-
             # Add Gpx import and viewer
             gpxCols = st.columns([1,4,1])
             with gpxCols[1]:
                 tourAttr["gpx"] = self.gpx.addGpx()
+
+                # Calculate some hard facts from gpx data
+                if len(tourAttr["gpx"])> 0 :
+                    tempGpx = self.gpx.calcFromGpx(tourAttr["gpx"])
+
+            # Write hard facts (or reuse from gpx data befor) 
+            cols2 = st.columns(3)
+            for i, at in enumerate(sorted(self.attributes)):
+                j = helpMod(i, 3)
+
+                # if theres a value from inserting gpx data ?
+                value = tempGpx[at] if at in tempGpx else ""
+
+                # the label contains the name and unit
+                label = f"{at} [{self.units[at]}]"
+
+                tourAttr[at] = cols2[j].text_input(label=label, value = value)
 
             # Add unique key
             tourAttr["unique"] = str(uuid.uuid1())
@@ -125,21 +151,25 @@ class TourWidget:
     
         return tourAttr
 
-
     # Add a caption statement
     def addCaption(self, txt: str):
         colsCaption = st.columns([1,0.5,1])
         colsCaption[1].caption(txt)
 
     # Add participate option
-    def participate(self, tour):
+    def participate(self, tour:dict, typ:str):
         # Add participation option
         cols = st.columns(5)
-        owner = st.session_state["login"]["credents"]["user"]
-        if owner in tour["participants"]:  # already participant
-            cols[-1].button("unparticipate", on_click = self.unparticipateCallback, args=(tour.copy(), ), key=str(tour["unique"]) + "unparticipate")
+        user = st.session_state["login"]["credents"]["user"]
+
+        # no option if tour owner and user are the same
+        if tour["owner"] == user:
+            return
+
+        if user in tour["participants"]:  # already participant
+            cols[-1].button("unparticipate", on_click = self.unparticipateCallback, args=(tour.copy(), ), key=str(tour["unique"]) + "unparticipate" + typ)
         else:  # not participant
-            cols[-1].button("participate", on_click = self.participateCallback, args=(tour.copy(), ) ,key=str(tour["unique"]) + "participate")
+            cols[-1].button("participate", on_click = self.participateCallback, args=(tour.copy(), ) ,key=str(tour["unique"]) + "participate" + typ)
 
     # Pariticpation callback
     def participateCallback(self, tour : dict):
@@ -158,3 +188,11 @@ class TourWidget:
         tour["participants"] = [p for p in tour["participants"] if p != user]
 
         self.base.insertNewTour(tour)
+
+    # Delete tour as owner callback
+    def deleteCallback(self, tour):
+        self.base.deleteTour(tour["unique"])
+
+    # Vicinity 
+    def vicinity(self):
+        pass
